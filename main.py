579938b3f77ceb27ca0524ec15904af721c10536
Jaslav789 <palacinka.papat0h@icloud.com>
2025-11-11 20:51:21 +0100

    Vytvoření result section.

--- main.py
+++ main.py
@@ -28,4 +28,23 @@ entry_height.grid(row=2, column=1)
 button = Button(root, text='Vypočítat')
 button.grid(row=3, column=1)
 
+# result seciton
+label_result_1 = Label(root, text='Číselný výsledek:')
+label_result_1.grid(row=4, column=0)
+
+label_user_result_1 = Label(root, text='DOPLNIT')
+label_user_result_1.grid(row=4, column=1)
+
+label_result_2 = Label(root, text='Textový výsledek:')
+label_result_2.grid(row=5, column=0)
+
+label_user_result_2 = Label(root, text='DOPLNIT')
+label_user_result_2.grid(row=5, column=1)
+
+label_count_text = Label(root, text='Počet uživatelů:')
+label_count_text.grid(row=6, column=0)
+
+label_count_number = Label(root, text='DOPLNIT')
+label_count_number.grid(row=6, column=1)
+
 root.mainloop()
