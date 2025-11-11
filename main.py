f1cf7a29c8147aa86b0a190e68d409771c8346c4
Jaslav789 <palacinka.papat0h@icloud.com>
2025-11-11 20:41:40 +0100

    Vytvoření weight a height section.

--- main.py
+++ main.py
@@ -3,7 +3,29 @@ import psycopg2
 
 root = Tk()
 root.title('Výpočet BMI')
-root.geometry('205x250')
+root.geometry('250x250')
 root.resizable(False, False)
+
+# general Label
+label_general = Label(root, text='Výpočet BMI')
+label_general.grid(row=0, column=1)
+
+# weight section
+label_weight = Label(root, text='Zadejte váhu (kg):')
+label_weight.grid(row=1, column=0)
+
+entry_weight = Entry(root)
+entry_weight.grid(row=1, column=1)
+
+# height section
+label_height = Label(root, text='Zadejte výšku (m):')
+label_height.grid(row=2, column=0)
+
+entry_height = Entry(root)
+entry_height.grid(row=2, column=1)
+
+# button
+button = Button(root, text='Vypočítat')
+button.grid(row=3, column=1)
 
 root.mainloop()
